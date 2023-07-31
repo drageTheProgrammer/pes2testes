1f236ee4d3ed76498a4ff69cff44877b5bf35490
drageNotAProgrammer <vinidrage@gmail.com>
2023-07-31 14:36:16 -0300

    teste
    dollar tem construtor que recebe int, times faz o amount * multiplier

--- TDD.java
+++ TDD.java
@@ -7,12 +7,22 @@ public class TDD{
        int amount = 10;
        Dollar(int amount) {}			
        void times(int multiplier) {}
+		int amount;
+		Dollar(int amount) {
+		  this.amount= amount;
+		}
+		void times(int multiplier) {
+		  amount= amount * multiplier;
+		}
     }	
 
     public void testMultiplication() {
        Dollar five = new Dollar(5);
        five.times(2);
        assertEquals(10, five.amount);
+		Dollar five = new Dollar(5);
+		five.times(2);
+		assertEquals(10, five.amount);
     }
 
 }
