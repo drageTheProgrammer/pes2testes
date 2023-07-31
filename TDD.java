64868d4d3b6c02a2010adf228fd4ce66d99bbc4f
drageNotAProgrammer <vinidrage@gmail.com>
2023-07-31 13:34:51 -0300

    Create TDD.java
    a função times não possui codigo, vai dar erro

---
+++ TDD.java
@@ -0,0 +1,18 @@
+public class TDD{
+	public static void main(String[] args){
+		testMultiplication();
+	}
+
+	class Dollar {
+       int amount = 10;
+       Dollar(int amount) {}			
+       void times(int multiplier) {}
+    }	
+
+    public void testMultiplication() {
+       Dollar five = new Dollar(5);
+       five.times(2);
+       assertEquals(10, five.amount);
+    }
+
+}
