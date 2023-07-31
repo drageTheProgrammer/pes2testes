b514b74e3cfafea3f9f10aee54ec69f10b3c3a92
drageNotAProgrammer <vinidrage@gmail.com>
2023-07-31 15:29:21 -0300

    testes 2
    melhora a função de teste

--- TDD.java
+++ TDD.java
@@ -9,19 +9,14 @@ public class TDD{
 		  this.amount= amount;
 		}
 		void times(int multiplier) {
-		  amount= amount * multiplier;
 			return new Dollar(amount * multiplier);
 		}
     }	
 
     public void testMultiplication() {
 		Dollar five = new Dollar(5);
-		five.times(2);
-		assertEquals(10, five.amount);
-		Dollar product = five.times(2);
-		assertEquals(10, product.amount);
-		product = five.times(3);
-		assertEquals(new Dollar(15), product);
+		assertEquals(new Dollar(10), five.times(2));
+		assertEquals(new Dollar(15), five.times(3));
     }
 
 }
