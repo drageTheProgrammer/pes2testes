aa01f9491b78e9b259806bbc14a82d104837f910
drageNotAProgrammer <vinidrage@gmail.com>
2023-07-31 16:01:18 -0300

    Classe franco
    foi adicionado a classe franco, e tambem um metodo para teste

--- TDD.java
+++ TDD.java
@@ -12,7 +12,22 @@ public class TDD{
 		void times(int multiplier) {
 			return new Dollar(amount * multiplier);
 		}
-    }	
+    }
+	
+		class Franc {   
+		private int amount;					
+			Franc(int amount) {      
+			this.amount= amount;
+		}					
+			Franc times(int multiplier)  {      
+			return new Franc(amount * multiplier);					
+		}   
+		public boolean equals(Object object) {					
+			Franc franc = (Franc) object;      
+			return amount == franc.amount;					
+		}					
+	}
+
 
     public void testMultiplication() {
 		Dollar five = new Dollar(5);
@@ -28,5 +43,11 @@ public class TDD{
 	public boolean equals(Object object)  {
 		Dollar dollar = (Dollar) object;
 		return amount == dollar.amount;
+	}
+	
+	public void testFrancMultiplication() {
+		Franc five = new Franc(5);
+		assertEquals(new Franc(10), five.times(2));
+		assertEquals(new Franc(15), five.times(3));
 	}
 }
