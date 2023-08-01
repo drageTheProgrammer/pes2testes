df2c9d3c1f819746d41f343c0c5bb4240b5101de
drageNotAProgrammer <vinidrage@gmail.com>
2023-08-01 11:33:51 -0300

    teste equal
    alterações no metodo de teste

--- TDD.java
+++ TDD.java
@@ -83,8 +83,6 @@ public class TDD{
 	public void testEquality() {
 		assertTrue(Money.Dollar(5).equals(Money.Dollar(5)));
 		assertFalse(Money.Dollar(5).equals(Money.Dollar(6)));
-		assertTrue(Money.Franc(5).equals(Money.Franc(5)));
-		assertFalse(Money.Franc(5).equals(Money.Franc(6)));
 		assertFalse(Money.Franc(5).equals(Money.Dollar(5)));
 	}
 	
